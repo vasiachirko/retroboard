544921dcf532a2ef4ab66ffff65dbe1181ebe3ac
vasiliiChirko <vasia_chirko@mail.ru>
2026-07-04 17:46:49 +0300

    Add basic router

--- app/Application.php
+++ app/Application.php
@@ -6,7 +6,20 @@ class Application
 {
     public function run(): void
     {
-        $this->render('home');
+        $uri = $_SERVER['REQUEST_URI'];
+
+        $this->handleRoute($uri);
+    }
+
+    private function handleRoute(string $uri): void
+    {
+        $path = trim($uri, '/');
+
+        if ($path === '') {
+            $path = 'home';
+        }
+
+        $this->render($path);
     }
 
     private function render(string $view): void
---
+++ views/about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+</head>
+<body>
+    <h1>О форуме RetroBoard</h1>
+    <p>Форум в стиле 2000-х</p>
+</body>
+</html>
